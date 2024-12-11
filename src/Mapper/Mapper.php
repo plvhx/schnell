720bdac8528facdfee806d652eb047176c70835a
plvhx <gandung@slackerc0de.us>
2024-12-11 22:47:03 +0700

    (src/Mapper/Mapper.php): Do major changes.

--- src/Mapper/Mapper.php
+++ src/Mapper/Mapper.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Schnell\Mapper;
 
+use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Http\Message\RequestInterface;
+use Schnell\Entity\Hydrator\HydratorInterface;
 
 /**
  * @author Paulus Gandung Prakosa <gandung@infradead.org>
@@ -17,12 +20,28 @@ final class Mapper implements MapperInterface
     private $entityManager;
 
     /**
+     * @var Psr\Http\Message\RequestInterface
+     */
+    private $request;
+
+    /**
+     * @var Doctrine\ORM\AbstractQuery
+     */
+    private $dql;
+
+    /**
+     * @var Schnell\Hydrator\HydratorInterface
+     */
+    private $hydrator;
+
+    /**
      * @param Doctrine\ORM\EntityManagerInterface $entityManager
      * @return static
      */
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->setEntityManager($entityManager);
+        $this->setDql(null);
     }
 
     /**
@@ -40,5 +59,106 @@ final class Mapper implements MapperInterface
     public function setEntityManager(EntityManagerInterface $entityManager): void
     {
         $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRequest(RequestInterface $request): void
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function withRequest(RequestInterface $request): MapperInterface
+    {
+        $ret = clone $this;
+        $ret->setRequest($request);
+        return $ret;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDql(): AbstractQuery|null
+    {
+        return $this->dql;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDql(AbstractQuery|null $dql): void
+    {
+        $this->dql = $dql;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function withDql(AbstractQuery|null $dql): MapperInterface
+    {
+        $ret = clone $this;
+        $ret->setDql($dql);
+        return $ret;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function runDql()
+    {
+        return $this->getDql()->getResult();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHydrator(): HydratorInterface
+    {
+        return $this->hydrator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setHydrator(HydratorInterface $hydrator): void
+    {
+        $this->hydrator = $hydrator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function withHydrator(HydratorInterface $hydrator): MapperInterface
+    {
+        $ret = clone $this;
+        $ret->setHydrator($hydrator);
+        return $ret;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function all(string $entityClass): array
+    {
+        $hydrator = $this->getHydrator();
+        $result = $this->getEntityManager()
+            ->getRepository($entityClass)
+            ->findAll();
+
+        return $hydrator === null
+            ? $result
+            : $hydrator->hydrate($result);
     }
 }
