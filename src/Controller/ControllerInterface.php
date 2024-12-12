7983e0e47788391fdb158f2a07a1f0d6819202b0
plvhx <gandung@slackerc0de.us>
2024-12-12 14:06:32 +0700

    (src/Controller/ControllerInterface.php): opcache.preload things.

--- src/Controller/ControllerInterface.php
+++ src/Controller/ControllerInterface.php
@@ -7,6 +7,14 @@ namespace Schnell\Controller;
 use Schnell\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 
+use function class_exists;
+
+// help opcache.preload discover always-needed symbols
+// phpcs:disable
+class_exists(ContainerInterface::class);
+class_exists(ResponseInterface::class);
+// phpcs:enable
+
 /**
  * @author Paulus Gandung Prakosa <gandung@infradead.org>
  */
