f1e445d7b132479a309bf07b78671e87bc06cc02
plvhx <gandung@slackerc0de.us>
2024-12-12 14:07:32 +0700

    (src/Controller/ControllerResolver.php): opcache.preload things.

--- src/Controller/ControllerResolver.php
+++ src/Controller/ControllerResolver.php
@@ -16,9 +16,25 @@ use Slim\Middleware\BodyParsingMiddleware;
 use Slim\Middleware\ErrorMiddleware;
 use Slim\Middleware\RoutingMiddleware;
 
+use function class_exists;
 use function is_a;
 use function is_array;
 use function sprintf;
+
+// help opcache.preload discover always-needed symbols
+// phpcs:disable
+class_exists(Route::class);
+class_exists(ControllerInterface::class);
+class_exists(ResponseInterface::class);
+class_exists(ServerRequestInterface::class);
+class_exists(MiddlewareInterface::class);
+class_exists(LoggerInterface::class);
+class_exists(RouteCollectorProxy::class);
+class_exists(RouteCollectorProxyInterface::class);
+class_exists(BodyParsingMiddleware::class);
+class_exists(ErrorMiddleware::class);
+class_exists(RoutingMiddleware::class);
+// phpcs:enable
 
 /**
  * @author Paulus Gandung Prakosa <gandung@infradead.org>
