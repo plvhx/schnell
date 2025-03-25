6766a2332afabc533c79b2d46e076c347033084f
plvhx <foobar.github@gmail.com>
2025-03-25 21:47:22 +0700

    (src/Attribute/Schema/): Add this directory, and do partial work on file Enum.php.

---
+++ src/Attribute/Schema/ChainEnum.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Schnell\Attribute\Schema;
+
+use Attribute;
+use Schnell\Attribute\AttributeInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <gandung@infradead.org>
+ */
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_PROPERTY)]
+class ChainEnum implements AttributeInterface
+{
+    /**
+     * @var string
+     */
+    private string $field;
+
+    /**
+     * @var array
+     */
+    private array $value;
+
+    /**
+     * @param string $field
+     * @param array $value
+     * @return static
+     */
+    public function __construct(string $field, array $value)
+    {
+        $this->field = $field;
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getField(): string
+    {
+        return $this->field;
+    }
+
+    /**
+     * @param string $field
+     * @return void
+     */
+    public function setField(string $field): void
+    {
+        $this->field = $field;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValue(): array
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param array $value
+     * @return void
+     */
+    public function setValue(array $value): void
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return 'schema.chainEnum';
+    }
+}
---
+++ src/Attribute/Schema/Enum.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Schnell\Attribute\Schema;
+
+use Attribute;
+use Schnell\Attribute\AttributeInterface;
