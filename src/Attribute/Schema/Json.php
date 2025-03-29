15459dadf1ba2a568082159985cf339e105b8665
plvhx <foobar.github@gmail.com>
2025-03-29 19:56:41 +0700

    (src/Attribute/Schema/Json.php): Adding this file.

---
+++ src/Attribute/Schema/Json.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Schnell\Attribute\Schema;
+
+use Attribute;
+use Schnell\Attribute\AttributeInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <gandung@infradead.org>
+ */
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_PROPERTY)]
+class Json implements AttributeInterface
+{
+    /**
+     * @var string
+     */
+    private string $name;
+
+    /**
+     * @param string $name
+     * @return static
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return 'schema.json';
+    }
+}
