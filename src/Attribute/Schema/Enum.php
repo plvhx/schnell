5e302ec71b8fd3d64886037b570e18233d71e25b
plvhx <foobar.github@gmail.com>
2025-03-29 10:17:17 +0700

    (src/Attribute/Schema/Enum.php): Completing the logic of this class attribute.

--- src/Attribute/Schema/Enum.php
+++ src/Attribute/Schema/Enum.php
@@ -6,3 +6,49 @@ namespace Schnell\Attribute\Schema;
 
 use Attribute;
 use Schnell\Attribute\AttributeInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <gandung@infradead.org>
+ */
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_PROPERTY)]
+class Enum implements AttributeInterface
+{
+    /**
+     * @var array
+     */
+    private array $value;
+
+    /**
+     * @param array $value
+     * @return static
+     */
+    public function __construct(array $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValue(): array
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param array $value
+     * @return void
+     */
+    public function setValue(array $value): void
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return 'schema.enum';
+    }
+}
